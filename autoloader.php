01be1580289ceeffccb99978da8ef024bd0ec51b
Zhaala <konrad.frysiak@gmail.com>
2017-04-04 18:32:16 +0200

    autoloader path

--- autoloader.php
+++ autoloader.php
@@ -1,6 +1,6 @@
 <?php
 
-if (file_exists($file = __DIR__ . '/vendor/autoload.php')) {
+if (file_exists($file = __DIR__ . '../../autoload.php')) {
     $loader = require $file;
     $loader->add('Repel', array(__DIR__.'/src/'));
     $loader->add('data', array(__DIR__.'/app/'));
